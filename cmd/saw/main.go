f25b007d03ff93b2fd3cc74f1013d3721179fb9d
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-03-23 18:27:14 -0700

    feat: add sawtools check-completion command for SubagentStop hook validation

--- cmd/saw/main.go
+++ cmd/saw/main.go
@@ -22,6 +22,7 @@ func main() {
 		newValidateCmd(),
 		newExtractContextCmd(),
 		newSetCompletionCmd(),
+		newCheckCompletionCmd(),
 		newMarkCompleteCmd(),
 		newSetImplStateCmd(),
 		newRunGatesCmd(),
