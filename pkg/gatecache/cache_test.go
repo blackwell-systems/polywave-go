06b9a8465aff08903b8a89d0d55cfe284de74719
Dayna Blackwell <blackwellsystems@protonmail.com>
2026-04-03 10:23:58 -0700

    Revert "fix: update cache_test.go to use result.CodeCacheMiss constant"
    This reverts commit d184535d8850d63df443fc38dc791fa6f9dbab1c.

--- pkg/gatecache/cache_test.go
+++ pkg/gatecache/cache_test.go
@@ -8,8 +8,6 @@ import (
 	"path/filepath"
 	"sync"
 	"testing"
-
-	"github.com/blackwell-systems/scout-and-wave-go/pkg/result"
 	"time"
 )
 
@@ -144,7 +142,7 @@ func TestCache_Get_ReturnsMissCodeOnExpiry(t *testing.T) {
 	if getResult.IsSuccess() {
 		t.Errorf("expected CACHE_MISS on expiry but got a hit: %v", getResult.GetData())
 	}
-	if len(getResult.Errors) == 0 || getResult.Errors[0].Code != result.CodeCacheMiss {
+	if len(getResult.Errors) == 0 || getResult.Errors[0].Code != "CACHE_MISS" {
 		t.Errorf("expected CACHE_MISS error code, got %v", getResult.Errors)
 	}
 	if len(getResult.Errors) > 0 && getResult.Errors[0].Severity == "fatal" {
@@ -175,7 +173,7 @@ func TestCache_TTLExpiry(t *testing.T) {
 	if getResult.IsSuccess() {
 		t.Errorf("expected CACHE_MISS due to TTL expiry but got a hit: %v", getResult.GetData())
 	}
-	if len(getResult.Errors) == 0 || getResult.Errors[0].Code != result.CodeCacheMiss {
+	if len(getResult.Errors) == 0 || getResult.Errors[0].Code != "CACHE_MISS" {
 		t.Errorf("expected CACHE_MISS error code, got %v", getResult.Errors)
 	}
 }
